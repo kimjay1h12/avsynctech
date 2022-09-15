2a0b4f5e10d07fc5922795bc3d511c9f569df664
unknown <olawaleadeit@gmail.com>
2022-09-15 22:37:56 +0100

    update

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,8 @@ const useStyles = makeStyles({
       padding: 10,
 
       ["@media (min-width : 1200px)"]: {
-        padding: "10vw",
+        paddingLeft: "6vw",
+        paddingRight: "6vw",
       },
     },
     "& .MuiGrid-item": {
@@ -35,7 +36,7 @@ function Header() {
     <div className={classes.root}>
       <Grid
         mt={8}
-        height="100%"
+        height="90vh"
         width="100vw"
         justifyContent="center"
         alignItems="center"
@@ -46,51 +47,50 @@ function Header() {
         gap={10}
         flexDirection="row"
       >
-        <Animator variant="slide" direction="right" delay={800} timeout={1000}>
-          <Grid item xs={11} md={5} lg={5}>
-            <Typography variant="h2" color="#fff" fontWeight={700}>
-              Get a <br />
-              Smater Life with us
-            </Typography>
-            <Typography mt={4} lineHeight={2} color="#fff" fontWeight="200px">
-              Our smart products are compatible with tens of thousands of other
-              devices and can be seamlessly integrated with popular platforms
-              like Google, Alexa, and Siri. Our smart products are compatible
-              with tens of thousands of other devices and can be seamlessly
-              integrated with popular platforms like Google, Alexa, and Siri.
-            </Typography>
+        <Grid item xs={11} md={5} lg={5}>
+          <Typography variant="h2" color="#fff" fontWeight={700}>
+            Get a <br />
+            Smater Life with us
+          </Typography>
+          <Typography mt={4} lineHeight={2} color="#fff" fontWeight="200px">
+            Our smart products are compatible with tens of thousands of other
+            devices and can be seamlessly integrated with popular platforms like
+            Google, Alexa, and Siri. Our smart products are compatible with tens
+            of thousands of other devices and can be seamlessly integrated with
+            popular platforms like Google, Alexa, and Siri.
+          </Typography>
 
-            <Grid className="right" flexDirection="row" mt={3}>
-              <Button
-                variant="outlined"
-                style={{
-                  background: "#1355FF",
-                  color: "#fff",
-                  fontSize: 12,
-                  padding: 16,
-                  borderRadius: 0,
-                  width: 150,
-                }}
-                size="lg"
-              >
-                {" "}
-                Get Started
-              </Button>{" "}
-              <Button
-                style={{
-                  background: "#fff",
-                  color: "#999",
-                  borderRadius: 0,
-                  fontSize: 12,
-                  padding: 16,
-                  width: 150,
-                }}
-              >
-                See video
-              </Button>
-            </Grid>
+          <Grid className="right" flexDirection="row" mt={3}>
+            <Button
+              variant="outlined"
+              style={{
+                background: "#1355FF",
+                color: "#fff",
+                fontSize: 12,
+                padding: 16,
+                borderRadius: 0,
+                width: 150,
+              }}
+              size="lg"
+            >
+              {" "}
+              Get Started
+            </Button>{" "}
+            <Button
+              style={{
+                background: "#fff",
+                color: "#999",
+                borderRadius: 0,
+                fontSize: 12,
+                padding: 16,
+                width: 150,
+              }}
+            >
+              See video
+            </Button>
           </Grid>
-        </Animator>
+        </Grid>
+
         <Animator variant="fade" delay={100} timeout={500}>
           <Grid
             item
