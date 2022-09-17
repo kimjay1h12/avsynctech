bda871c87dddade3268f2980ded70af0c3eeadc6
unknown <olawaleadeit@gmail.com>
2022-09-18 00:47:52 +0100

    update

--- components/Map.js
+++ components/Map.js
@@ -3,7 +3,7 @@ import Iframe from "react-iframe";
 
 function Map() {
   return (
-    <div>
+    <div style={{ width: "100%" }}>
       {" "}
       <Iframe
         url="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=avsynctech+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
