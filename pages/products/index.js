772fc88fb5f6d7342a285dc67879760498792414
unknown <olawaleadeit@gmail.com>
2022-09-21 19:15:40 +0100

    updated email

--- pages/products/index.js
+++ pages/products/index.js
@@ -27,9 +27,9 @@ function Index() {
     <MainLayout>
       <form ref={form} onSubmit={sendEmail} style={{ margin: "150px" }}>
         <label>Name</label>
-        <input type="text" name="user_name" />
+        <input type="text" name="name" />
         <label>Email</label>
-        <input type="email" name="user_email" />
+        <input type="email" name="email" />
         <label>Message</label>
         <textarea name="message" />
         <input type="submit" value="Send" />
