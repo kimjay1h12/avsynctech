10decde48df9842098d59773c80f3deb7ba0c652
unknown <olawaleadeit@gmail.com>
2022-10-01 10:08:20 +0100

    update

--- pages/contactus/index.js
+++ pages/contactus/index.js
@@ -12,6 +12,8 @@ import {
 import React from "react";
 import Animator from "../../components/Animator";
 import Map from "../../components/Map";
+import Animator from "./Animator";
+import { firebaseConfig } from "../../Backend/Firebase";
 import { Mail, Rowing } from "@mui/icons-material";
 import {
   AccountCircle,
@@ -22,11 +24,32 @@ import {
 } from "@mui/icons-material";
 import MainLayout from "../../layouts/MainLayout";
 import { makeStyles } from "@mui/styles";
-import Card1 from "../../components/Card1";
+
 import Footer from "../../components/Footer";
-import ContactUs from "../../components/Contactus";
+import ContactUs from "../components/Section";
 const headerimg = "/img/contactheader.png";
 const UseStyles = makeStyles({
+  form: {
+    marginTop: 100,
+    transition: "0.5s",
+    minHeight: "30vh",
+    "& .MuiGrid-container": {
+      padding: 10,
+
+      ["@media (min-width : 1200px)"]: {
+        paddingLeft: 120,
+        paddingRight: 120,
+        gap: "180px",
+      },
+    },
+    "& .MuiGrid-item": {
+      marginTop: 120,
+
+      ["@media (min-width : 1200px)"]: {
+        marginTop: 0,
+      },
+    },
+  },
   root: {
     marginTop: 100,
     transition: "0.5s",
@@ -51,6 +74,47 @@ const UseStyles = makeStyles({
 });
 function index() {
   const classes = UseStyles();
+
+  const [country, setCountry] = useState("Country");
+  const options = useMemo(() => countryList().getData(), []);
+  const changeHandler = (country) => {
+    setCountry(country);
+  };
+  const [showButton, setShowButton] = React.useState(true);
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [value, setValue] = useState();
+  const [message, setMessage] = useState();
+  const form = React.useRef();
+  const app = initializeApp(firebaseConfig);
+
+  function writeUserData() {
+    const db = getDatabase(app);
+    push(ref(db, "users/"), {
+      name,
+      email,
+      message,
+    });
+    alert("Your Messages will be delivered");
+  }
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_wkr80os",
+        "template_tw0xx35",
+        form.current,
+        "cfb2n4hwNsmbzyQDB"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {}
+      );
+    setValue("");
+  };
   return (
     <MainLayout title="Contact Us" description="Beep Us with Your messages">
       {" "}
@@ -77,7 +141,160 @@ function index() {
           </Typography>
         </header>
         <section>
-          <ContactUs />
+          <div className={classes.form}>
+            <Grid
+              mt={mt}
+              width="100vw"
+              justifyContent="center"
+              alignItems="center"
+              container
+              xs={12}
+              md={12}
+              lg={12}
+            >
+              <Grid item xs={11} md={4} lg={4}>
+                <Typography
+                  mt={2}
+                  lineHeight={2}
+                  color="#f90a"
+                  variant="caption"
+                >
+                  Contact-Us
+                </Typography>
+                <Typography
+                  variant="h4"
+                  fontSize={35}
+                  color="#fff"
+                  fontWeight={700}
+                >
+                  Get In Touch
+                </Typography>
+                <Grid flexDirection="column">
+                  <form
+                    ref={form}
+                    style={{
+                      gap: 10,
+                      mt: 50,
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                    onSubmit={sendEmail}
+                  >
+                    <FormControl root variant="outlined" fullWidth="100%">
+                      <OutlinedInput
+                        sx={{
+                          background: "#fff",
+                          margin: "0",
+                          width: "100%",
+                          borderRadius: 2,
+                        }}
+                        endAdornment={
+                          <InputAdornment position="end">
+                            <IconButton>
+                              <AccountCircle />
+                            </IconButton>
+                          </InputAdornment>
+                        }
+                        placeholder="Full name"
+                        type="text"
+                        name="name"
+                        value={value}
+                        onChange={(e) => {
+                          setName(e.target.value);
+                        }}
+                      />
+                    </FormControl>
+                    <FormControl root variant="outlined" fullWidth="100%">
+                      <OutlinedInput
+                        sx={{
+                          background: "#fff",
+                          margin: "0",
+                          width: "100%",
+                          borderRadius: 2,
+                        }}
+                        endAdornment={
+                          <InputAdornment position="end">
+                            <IconButton>
+                              <Mail />
+                            </IconButton>
+                          </InputAdornment>
+                        }
+                        placeholder="Email"
+                        name="email"
+                        value={value}
+                        onChange={(e) => {
+                          setEmail(e.target.value);
+                        }}
+                      />
+                    </FormControl>
+                    <FormControl root variant="outlined" fullWidth="100%">
+                      <OutlinedInput
+                        sx={{
+                          background: "#fff",
+                          margin: "0",
+                          width: "100%",
+                          minHeight: "100px",
+                          display: "flex",
+                          alignItems: "flex-start",
+                          lineBreak: "auto",
+                          borderRadius: 2,
+                        }}
+                        placeholder="Message"
+                        type="text"
+                        value={value}
+                        name="message"
+                        onChange={(e) => {
+                          setMessage(e.target.value);
+                        }}
+                      />
+                    </FormControl>
+
+                    <Grid
+                      width="100%"
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <Button
+                        size="large"
+                        type="submit"
+                        variant="outlined"
+                        onClick={writeUserData}
+                        sx={{
+                          width: "100%",
+
+                          background: "#fff",
+                          color: "#999",
+                          minHeight: 52,
+                        }}
+                      >
+                        Contact Us Now
+                      </Button>
+                    </Grid>
+                  </form>
+                </Grid>
+              </Grid>
+              <Animator variant="fade" delay={100} timeout={500}>
+                <Grid
+                  item
+                  xs={12}
+                  md={5}
+                  lg={5}
+                  height="400px"
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Map />
+                </Grid>
+              </Animator>
+            </Grid>
+          </div>
           <Grid
             container
             mt={10}
